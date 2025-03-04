eb323318da12582412ec3a1da41a9b0a280e5be9
choycoy <passionfruits479@gmail.com>
2025-03-04 20:35:35 +0900

    feat: 헤더 및 메뉴 모달 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Router from "./Router";
+import { OverlayProvider } from "overlay-kit";
 
 function App() {
-  return <Router />;
+  return (
+    <BrowserRouter>
+      <OverlayProvider>
+        <Router />
+      </OverlayProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
