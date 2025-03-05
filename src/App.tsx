5ce436066db489e33c7ce78e02ff39d467ac0490
choycoy <passionfruits479@gmail.com>
2025-03-05 14:19:19 +0900

    feat: added react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./Router";
 import { OverlayProvider } from "overlay-kit";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        staleTime: 5 * 60 * 1000,
+        retry: 1,
+      },
+    },
+  });
   return (
-    <BrowserRouter>
-      <OverlayProvider>
-        <Router />
-      </OverlayProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <OverlayProvider>
+          <Router />
+        </OverlayProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
