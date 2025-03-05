e94057de6103dc2c4e15736dbafe3d9e5524e668
choycoy <passionfruits479@gmail.com>
2025-03-05 15:05:46 +0900

    feat: enabled msw

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_URL = process.env.NODE_ENV === "development" ? "http://localhost:5713" : process.env.VITE_SERVER_URL;
+const SERVER_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : process.env.VITE_SERVER_URL;
 const client = axios.create({
   baseURL: SERVER_URL,
   withCredentials: true,
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
     svgr({
-    svgrOptions: {
-      exportType: 'default',
-      ref: true,
-      svgo: false,
-      titleProp: true,
-    },
-    include: '**/*.svg',
-    }),],
-    resolve: {
-      alias: [
-        { find: "@", replacement: "/src" }
-      ],
-    },
-})
+      svgrOptions: {
+        exportType: "default",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
+  ],
+  optimizeDeps: {
+    include: ["msw"],
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  server: {
+    port: 3000,
+  },
+});
