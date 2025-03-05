ad2c470e066d669e6b4491758f35d20579ad8913
choycoy <passionfruits479@gmail.com>
2025-03-05 23:47:00 +0900

    feat: added Sentry to log errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import worker from "@/mocks/browser";
+import * as Sentry from "@sentry/react";
 
 async function startWorker() {
   await worker.start({
     onUnhandledRequest: "bypass",
   });
 }
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DNS,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", import.meta.env.VITE_SERVER_URL],
+  replaysSessionSampleRate: 0,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 startWorker().then(() => {
   createRoot(document.getElementById("root")!).render(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
@@ -5,26 +6,32 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    svgr({
-      svgrOptions: {
-        exportType: "default",
-        ref: true,
-        svgo: false,
-        titleProp: true,
-      },
-      include: "**/*.svg",
-    }),
-  ],
+  plugins: [react(), tailwindcss(), svgr({
+    svgrOptions: {
+      exportType: "default",
+      ref: true,
+      svgo: false,
+      titleProp: true,
+    },
+    include: "**/*.svg",
+  }), sentryVitePlugin({
+    org: "studit-y3",
+    project: "javascript-react"
+  })],
+
   optimizeDeps: {
     include: ["msw"],
   },
+
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
+
   server: {
     port: 3000,
   },
+
+  build: {
+    sourcemap: true
+  }
 });
