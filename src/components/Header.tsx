62b5853880c5bcbfface92549cd9daf650cb5d1f
choycoy <passionfruits479@gmail.com>
2025-03-06 22:24:27 +0900

    fix: Replace header logo with Link tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import logoImg from "@/assets/imgs/logo.png";
 import MenuIcon from "@/assets/icons/hamburger-menu.svg";
 import { overlay } from "overlay-kit";
 import Menu from "./Menu";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   const onClick = () => {
@@ -11,7 +12,9 @@ export default function Header() {
   };
   return (
     <header className="max-w-3xl w-full h-[52px] bg-main px-4 flex items-center justify-between">
-      <img src={logoImg} alt="스터딧 로고" className="w-[87px] h-[18px]" />
+      <Link to={"/"}>
+        <img src={logoImg} alt="스터딧 로고" className="w-[87px] h-[18px]" />
+      </Link>
       <button aria-label="메뉴" onClick={onClick} className="cursor-pointer">
         <MenuIcon alt="메뉴" />
       </button>
