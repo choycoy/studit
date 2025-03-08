5f0f39b1f4510f15248560821fccb720464f46c9
choycoy <passionfruits479@gmail.com>
2025-03-09 00:12:53 +0900

    fix: nav 메뉴 항목 클릭시 메뉴 닫기

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,10 @@
-import { Link } from "react-router-dom";
 import kakaoLogo from "@/assets/imgs/kakao-logo.png";
 import CloseIcon from "@/assets/icons/close.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function Menu({ isOpen, close }: { isOpen: boolean; close: () => void }) {
+  const navigate = useNavigate();
+
   return (
     <section
       className={`fixed top-0 left-0 w-full h-full bg-main text-white z-50 transition-transform duration-300 flex flex-col justify-between ${
@@ -16,18 +18,39 @@ export default function Menu({ isOpen, close }: { isOpen: boolean; close: () => 
       </h1>
       <ul className="ml-10 flex flex-col gap-y-5 text-lg">
         <li className="hover:text-grey-01 transition-colors">
-          <Link to={"/"}>홈</Link>
+          <button
+            onClick={() => {
+              close();
+              navigate("/");
+            }}
+          >
+            홈
+          </button>
         </li>
         <li className="hover:text-grey-01 transition-colors">
-          <Link to={"/study-lounge"}>스터디 라운지</Link>
+          <button
+            onClick={() => {
+              close();
+              navigate("/study-lounge");
+            }}
+          >
+            스터디 라운지
+          </button>
         </li>
         <li className="hover:text-grey-01 transition-colors">
-          <Link to={"/my-page"}>마이 페이지</Link>
+          <button
+            onClick={() => {
+              close();
+              navigate("/my-page");
+            }}
+          >
+            마이 페이지
+          </button>
         </li>
       </ul>
-      <button className="cursor-pointer flex justify-center w-[343px] bg-[#FEE500] h-[44px] items-center mx-4 rounded-full mb-[52px] hover:grayscale-[35%]">
+      <button className="cursor-pointer flex justify-center w-[343px] bg-[#FEE500] h-[44px] items-center mx-auto rounded-full mb-[52px] hover:grayscale-[35%]">
         <img alt="카카오 로고" src={kakaoLogo} />
-        <span className="ml-2 text-black opacity-85 font-bold">3초만에 카카오톡으로 시작하기</span>
+        <span className="ml-2 text-black opacity-85 font-medium">3초만에 카카오톡으로 시작하기</span>
       </button>
       <button className="fixed top-6 right-5 z-50 cursor-pointer" aria-label="메뉴 닫기" onClick={close}>
         <CloseIcon alt="메뉴 닫기" className="hover:text-grey-01 transition-colors" />
