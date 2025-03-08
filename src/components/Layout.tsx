d643412517f0daad5cf0daf919bb87cc345d1e59
choycoy <passionfruits479@gmail.com>
2025-03-08 18:33:11 +0900

    fix: 기본 color index.css가 아닌 layout main 태그에 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <div className="min-h-screen w-full h-full">
       <Header />
-      <main className="h-full w-full max-w-3xl">
+      <main className="h-full w-full max-w-3xl text-main">
         <Outlet />
       </main>
     </div>
