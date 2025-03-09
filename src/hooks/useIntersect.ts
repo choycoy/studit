60f9e26727654a21092421740f41c3e2e686266a
choycoy <passionfruits479@gmail.com>
2025-03-09 20:00:58 +0900

    feat: 공용 커스텀 무한 스크롤훅 useIntersect 생성

---
+++ src/hooks/useIntersect.ts
@@ -0,0 +1,34 @@
+import { InfiniteQueryObserverResult } from "@tanstack/react-query";
+import { useCallback, useEffect, useState } from "react";
+
+export default function useIntersect({
+  threshold = 0,
+  hasNextPage,
+  fetchNextPage,
+}: {
+  threshold?: number;
+  hasNextPage: boolean;
+  fetchNextPage: () => Promise<InfiniteQueryObserverResult>;
+}) {
+  const [target, setTarget] = useState<HTMLLIElement | null>(null);
+
+  const observerCallback = useCallback<IntersectionObserverCallback>(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      });
+    },
+    [fetchNextPage, hasNextPage],
+  );
+
+  useEffect(() => {
+    if (!target) return;
+    const observer = new IntersectionObserver(observerCallback, { threshold });
+    observer.observe(target);
+    return () => observer.unobserve(target);
+  }, [observerCallback, threshold, target]);
+
+  return { setTarget };
+}
