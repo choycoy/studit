0f04f20ff34308c224aa0726801708f3cf6aa28a
choycoy <passionfruits479@gmail.com>
2025-03-12 16:42:16 +0900

    design: changed header colour

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import logoImg from "@/assets/imgs/logo.png";
 import MenuIcon from "@/assets/icons/hamburger-menu.svg";
 import { overlay } from "overlay-kit";
 import Menu from "./Menu";
@@ -11,9 +10,9 @@ export default function Header() {
     });
   };
   return (
-    <header className="max-w-3xl w-full h-[52px] bg-main px-4 flex items-center justify-between">
+    <header className="max-w-3xl w-full h-[52px] bg-white text-main px-4 flex items-center justify-between">
       <Link to={"/"}>
-        <img src={logoImg} alt="스터딧 로고" className="w-[87px] h-[18px]" />
+        <span className="bagel-fat-one-regular text-2xl">STUDIT</span>
       </Link>
       <button aria-label="메뉴" onClick={onClick} className="cursor-pointer">
         <MenuIcon alt="메뉴" />
